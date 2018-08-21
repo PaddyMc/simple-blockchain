5fc6d3f86f73d08932fb2c702cd6bfa235d73481
unknown <paddymchale@hotmail.com>
2018-08-21 19:38:09 +0100

    simple js blockchain

---
+++ main.js
@@ -0,0 +1,76 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+    constructor(timestamp, data, previousHash = '') {
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("BLOCK MINED: " + this.hash);
+    }
+}
+
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
+    }
+
+    createGenesisBlock() {
+        return new Block("01/01/2018", "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let simpleBC = new Blockchain();
+
+console.log('Mining block 1');
+simpleBC.addBlock(new Block(1, "20/07/2018", { amount: 4 }));
+
+console.log('Mining block 2');
+simpleBC.addBlock(new Block(2, "20/07/2018", { amount: 8 }));
+
+// console.log(JSON.stringify(simpleBC, null, 4));
+
+// console.log('Blockchain valid? ' + simpleBC.isChainValid());
+
+// simpleBC.chain[1].data = { amount: 100 };
+
+// console.log("Blockchain valid? " + simpleBC.isChainValid());
